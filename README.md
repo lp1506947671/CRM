d8dd85af2f6ed13b5d63f14b7262afacd578b0a7
lp1506947671 <1506946771@qq.com>
2021-07-01 08:21:39 +0800

    录入rbac相关信息

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 1.第三方包的版本
 ```requirements.txt
 Django==3.2
+PyMySQL==0.10.0
 ```
+
+
+2.数据库
+```sql
+127.0.0.1/crm
+root:666666
+```
+3.数据库迁移
+```shell script
+python manage.py makemigrations
+python manage.py migrate
+```
+**注意**
+> 在CRM\crm\crm\__init__.py中添加
+```python
+import pymysql
+pymysql.version_info = (1, 4, 13, "final", 0)
+pymysql.install_as_MySQLdb()
+```
+4.创建超级用户
+```shell script
+# 设置密码
+python manage.py createsuperuser 
+# 修改密码
+python manage.py changepassword 用户名
+```
+用户名:jason 邮件:jason@163.com 密码:yyds666666
--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rbac",
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crm',
+        'USER': 'root',
+        'PASSWORD': '666666',
+        'HOST': '127.0.0.1',
+        'PORT': 3306
     }
 }
 
@@ -103,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/shanghai'
 
 USE_I18N = True
 
