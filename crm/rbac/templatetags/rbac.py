c07acb4d81b636edc2c0da842cace5e4bcbf8dc7
lp1506947671 <1506946771@qq.com>
2021-07-25 11:20:57 +0800

    实现一级图标高亮效果

--- crm/rbac/templatetags/rbac.py
+++ crm/rbac/templatetags/rbac.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import re
+
 from django.conf import settings
 from django.template import Library
 
@@ -9,6 +11,10 @@ register = Library()
 @register.inclusion_tag('../templates/menu.html')
 def menu(request):
     menu_list = request.session.get(settings.MENU_SESSION_KEY)
+    for item in menu_list:
+        regex = f"^{item['url']}$"
+        if re.match(regex, request.path_info):
+            item['class'] = 'active'
     return {
         'menu_list': menu_list
     }
