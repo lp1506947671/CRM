03fb6a4380e2797c3984fa1cf4b7441cb994fcde
lp1506947671 <1506946771@qq.com>
2021-08-01 23:35:02 +0800

    权限分配之角色的删除

--- crm/rbac/views/role.py
+++ crm/rbac/views/role.py
@@ -39,6 +39,11 @@ def role_edit(request, pk):
     return render(request, "role_change.html", {"form": role_form})
 
 
-def role_del(request):
-    ...
-    return
+def role_del(request, pk):
+    origin_url = reverse("rbac:role_list")
+    if request.method == "GET":
+        return render(request, "role_delete.html", {'origin_url ': origin_url})
+    obj = Role.objects.filter(id=pk).delete()
+    if not obj:
+        return HttpResponse("角色不存在")
+    return redirect(origin_url)
