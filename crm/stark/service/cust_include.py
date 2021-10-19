d536d1d28a0a9915e5811013a454490e13b7f430
lp1506947671 <1506946771@qq.com>
2021-10-19 08:46:41 +0800

    [描述]:列表页面: 1.添加按钮显示和url 2.添加功能实现 [类型]:需求开发 [提交人]:Jason

--- crm/stark/service/cust_include.py
+++ crm/stark/service/cust_include.py
@@ -1,25 +1,77 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import functools
 from types import FunctionType
 
+from django import forms
 from django.conf.urls import url
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, QueryDict
+from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from stark.utils.pagination import Pagination
 
 
+class StarkModelForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(StarkModelForm, self).__init__(*args, **kwargs)
+        # 统一给ModelForm生成字段添加样式
+        for name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
 class StarkHandler:
     display_list = []
     per_page_count = 1
+    has_add_btn = True  # 是否启用添加按钮
+    model_form_class = None
 
     def __init__(self, model_class, prev):
         self.stark = stark
         self.model_class = model_class
         self.prev = prev
+        self.request = None
+
+    def get_model_form_class(self):
+        if self.model_form_class:
+            return self.model_form_class
+
+        class DynamicModelForm(StarkModelForm):
+            class Meta:
+                model = self.model_class
+                fields = "__all__"
+
+        return DynamicModelForm
+
+    def reverse_add_url(self):
+        name = f"{self.stark.namespace}:{self.url_name('add')}"
+        base_url = reverse(name)
+        if not self.request.GET:
+            add_url = base_url
+        else:
+            param = self.request.GET.urlencode()
+            new_query_dict = QueryDict(mutable=True)
+            new_query_dict['_filter'] = param
+            add_url = f"{base_url}?{new_query_dict.urlencode()}"
+        return add_url
+
+    def reverse_list_url(self):
+        name = f"{self.stark.namespace}:{self.url_name('list')}"
+        base_url = reverse(name)
+        param = self.request.GET.get('_filter')
+        if not param:
+            return base_url
+        return f"{base_url}?{param}"
+
+    def save(self, form, is_update=False):
+        """在使用ModelForm保存数据之前预留的钩子方法"""
+        form.save()
+
+    def get_add_btn(self):
+        if self.has_add_btn:
+            return "<a class='btn btn-primary' href='%s'>添加</a>" % self.reverse_add_url()
+        return None
 
     def get_list_display(self):
         """给予用户对表的列的自定义扩展,例如：根据用户的不同显示不同的列"""
@@ -40,7 +92,16 @@ class StarkHandler:
         return mark_safe(f'<a href="{reverse(name, args=(obj.pk,))}">删除</a>')
 
     def add_view(self, request):
-        return HttpResponse("增加")
+        model_form_class = self.get_model_form_class()
+        if request.method == 'GET':
+            form = model_form_class()
+            return render(request, 'change.html', {'form': form})
+        form = model_form_class(data=request.POST)
+        if form.is_valid():
+            self.save(form, is_update=False)
+            # 在数据库保存成功后，跳转回列表页面(携带原来的参数)。
+            return redirect(self.reverse_list_url())
+        return render(request, 'change.html', {'form': form})
 
     def delete_view(self, request, pk):
         return HttpResponse("删除")
@@ -86,10 +147,13 @@ class StarkHandler:
                 tr_list.append(row)
 
             body_list.append(tr_list)
+        # 添加按钮
+        add_btn = self.get_add_btn()
         result = {'header_list': header_list,
                   'body_list': body_list,
                   "current_page_num": current_page_num,
-                  'pager': pager
+                  'pager': pager,
+                  'add_btn': add_btn
                   }
         return render(request, 'list.html', result)
 
@@ -103,12 +167,20 @@ class StarkHandler:
     def extra_url():
         return []
 
+    def wrapper(self, func):
+        @functools.wraps(func)
+        def inner(request, *args, **kwargs):
+            self.request = request
+            return func(request, *args, **kwargs)
+
+        return inner
+
     def get_urls(self):
         patterns = [
-            url(r"^list/$", self.list_view, name=self.url_name("list")),
-            url(r"^add/$", self.add_view, name=self.url_name("add")),
-            url(r"^change/(\d+)$", self.change_view, name=self.url_name("change")),
-            url(r"^del/(\d+)/$", self.delete_view, name=self.url_name("del")),
+            url(r"^list/$", self.wrapper(self.list_view), name=self.url_name("list")),
+            url(r"^add/$", self.wrapper(self.add_view), name=self.url_name("add")),
+            url(r"^change/(\d+)$", self.wrapper(self.change_view), name=self.url_name("change")),
+            url(r"^del/(\d+)/$", self.wrapper(self.delete_view), name=self.url_name("del")),
         ]
         patterns.extend(self.extra_url())
         return patterns
